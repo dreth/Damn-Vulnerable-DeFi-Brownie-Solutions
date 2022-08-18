eabd4e46ed7c54dfd09068f4780566bd1b1993a3
dreth <github@dac.ac>
2022-08-19 00:20:12 +0200

    extend fix libs to fix all licenses in a folder

--- fix_libs.py
+++ fix_libs.py
@@ -1,21 +1,50 @@
 import os
 
 # contracts without license, to avoid compiler complaints
-files_to_add_license_to = {
-    'IUniswapV2Pair.sol':'.brownie/packages/Uniswap/v2-core@1.0.1/contracts/interfaces/',
-    'UniswapV2Library.sol':'.brownie/packages/Uniswap/v2-periphery@1.0.0-beta.0/contracts/libraries/'}
+folders_add_license = ['.brownie/packages/Uniswap/']
 
-for contract, path in files_to_add_license_to.items():
-    with open(f'/home/{os.environ["USER"]}/{path}/{contract}','r') as f:
-        contents = f.read()
-        first_line = contents.splitlines()[0]
+# go through all folders in folders_add_license and add a license to all contracts within that folder
+for folder in folders_add_license:
+    # path to folder
+    path = f'/home/{os.environ["USER"]}/{folder}'
 
-    with open(f'/home/{os.environ["USER"]}/{path}/{contract}','w') as f:
-        if '// SPDX-License-Identifier: GPL-v3.0' not in first_line:
-            f.truncate(0)
-            f.write(f'// SPDX-License-Identifier: GPL-v3.0\n{contents}')
-        else:
-            f.write(contents)
+    # loop recursively over all files in the path
+    for root, dirs, files in os.walk(path):
+
+        # license used for all contracts in the folder
+        license_used = None
+        files_without_license = {}
+
+        for f in files:
+            
+            # only match soliditycontracts
+            if f.endswith(".sol"):
+
+                # check if it has a license
+                with open(os.path.join(root,f),'r') as contract:
+                    contents = contract.read()
+                    first_line = contents.splitlines()[0]
+
+                    # if file does not have a license add the file to the list
+                    if 'SPDX-License-Identifier' not in first_line:
+                        files_without_license[os.path.join(root,f)] = contents
+
+                    # save which license if used if license_used is empty
+                    if not license_used and 'SPDX-License-Identifier' in first_line:
+                        license_used = first_line
+
+        # loop over files without a license
+        for filepath, contents in files_without_license.items():
+
+            # add the license to the file
+            with open(filepath,'w') as contract:
+                contract.truncate(0)
+                
+                # if we saved a license, use that, otherwise just use GPL v3
+                if license_used:
+                    contract.write(f'{license_used}\n{contents}')
+                else:
+                    contract.write(f'// SPDX-License-Identifier: GPL-v3.0\n{contents}')
 
 
 # files with solidity pragma statement that makes the compiler complain
